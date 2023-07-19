522e1b491509863536fcb5f3e36b8fb063b94b34
Ramzy Syahrul R <ramzyrsr@Ramzys-MacBook-Air.local>
2023-07-20 03:39:48 +0700

    init entities config

---
+++ src/config/entities.config.ts
@@ -0,0 +1,8 @@
+import { Banks } from 'src/banks/banks.entity';
+import { Branch } from 'src/branch/branch.entity';
+
+export const MMFPROD_Entities = [Banks];
+
+export const HCM_Entities = [Branch];
+
+export const HCM_RECRUITMENT_Entities = [];
