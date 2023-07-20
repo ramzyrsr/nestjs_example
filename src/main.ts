bbd79fc26902bd00ebfb62d2eea9fff55efa09d9
Ramzy Syahrul R <ramzyrsr@Ramzys-MacBook-Air.local>
2023-07-20 10:19:28 +0700

    add whitelist and transform

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(3000);
 
