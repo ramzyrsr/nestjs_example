115afed8386530de9bd283a1449e499d692d0910
Ramzy Syahrul R <ramzyrsr@Ramzys-MacBook-Air.local>
2023-07-20 10:20:19 +0700

    change request payload to using dto

--- src/branch/branch.controller.ts
+++ src/branch/branch.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Get } from '@nestjs/common';
 import { BranchService } from './branch.service';
+import { GetAllBanksDto } from './dto/get-banks.dto';
 
 @Controller('hcm/api/agent')
 export class BranchController {
   constructor(private branchService: BranchService) {}
 
   @Get('/getListAllBranch')
-  getAllBranch(@Body() employee_id: string): Promise<object> {
-    return this.branchService.getAllBranch(employee_id);
+  getAllBranch(@Body() banks: GetAllBanksDto): Promise<object> {
+    return this.branchService.getAllBranch(banks.employee_id);
   }
 }
