3824b981e45fb3d42462436c4824d7e999a80ae8
Ramzy Syahrul R <ramzyrsr@Ramzys-MacBook-Air.local>
2023-07-20 10:24:13 +0700

    change from GetAllBanksDto to GetAllBanksDto

--- src/branch/branch.controller.ts
+++ src/branch/branch.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Get } from '@nestjs/common';
 import { BranchService } from './branch.service';
-import { GetAllBanksDto } from './dto/get-banks.dto';
+import { GetAllBranchDto } from './dto/get-branch.dto';
 
 @Controller('hcm/api/agent')
 export class BranchController {
   constructor(private branchService: BranchService) {}
 
   @Get('/getListAllBranch')
-  getAllBranch(@Body() banks: GetAllBanksDto): Promise<object> {
+  getAllBranch(@Body() banks: GetAllBranchDto): Promise<object> {
     return this.branchService.getAllBranch(banks.employee_id);
   }
 }
